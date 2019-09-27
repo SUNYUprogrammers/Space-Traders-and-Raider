8dd72651fbd6cc3a5bf66a83d94658e322060c7b
John Harlan <harlanj@sunypoly.edu>
2019-09-27 15:55:38 -0400

    Started creating class for systems and sectors

---
+++ Space Traders and Raiders/Assets/Scripts/Map/Sector.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector : MonoBehaviour
+{
+    StarSystem[] systems;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        systems = new StarSystem[Random.Range(4,4)];
+        for(int i = 0; i < systems.Length; i++)
+        {
+            systems[i] = new StarSystem();
+
+            for (int j = 0; j < i; j++)
+            {
+                while (systems[i].getPosition() == systems[j].getPosition())
+                {
+                    systems[i].setPosition(Random.Range(0, 3), Random.Range(0, 3));
+                }
+            }
+        }
+
+        for(int i = 0; i < systems.Length; i++)
+        {
+            Debug.Log(systems[i].getPosition());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Space Traders and Raiders/Assets/Scripts/Map/StarSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSystem
+{
+    public enum SystemType
+    {
+        YELLOW = 55, GREEN = 30, BLUE = 12, RED = 3
+    }
+
+    private Vector2Int position = Vector2Int.zero;
+
+    private SystemType type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int prob = Random.Range(1, 100);
+        if(prob <= (int)SystemType.YELLOW)
+        {
+            this.type = SystemType.YELLOW;
+        } else if (prob <= (int)SystemType.GREEN)
+        {
+            this.type = SystemType.GREEN;
+        } else if (prob <= (int)SystemType.BLUE)
+        {
+            this.type = SystemType.BLUE;
+        } else if (prob <= (int)SystemType.RED)
+        {
+            this.type = SystemType.RED;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool setPosition(int x, int y)
+    {
+        bool hasSet = false;
+        if(x >= 0 && y >= 0)
+        {
+            position.Set(x, y);
+            hasSet = true;
+        }
+
+        return hasSet;
+    }
+
+    public bool setPosition(Vector2Int v)
+    {
+        bool hasSet = false;
+        if (v.x >= 0 && v.y >= 0)
+        {
+            position.Set(v.x, v.y);
+            hasSet = true;
+        }
+
+        return hasSet;
+    }
+
+    public Vector2Int getPosition()
+    {
+        return this.position;
+    }
+}
