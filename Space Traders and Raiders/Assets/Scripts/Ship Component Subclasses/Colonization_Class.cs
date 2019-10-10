128260eca673ed3346a19f59b45aa042b1179672
YsevniaBaladon <54846816+YsevniaBaladon@users.noreply.github.com>
2019-10-10 15:07:32 -0400

    Add files via upload

---
+++ Space Traders and Raiders/Assets/Scripts/Ship Component Subclasses/Colonization_Class.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colonization_Class : Component_Class
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        type = "Colonization";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
