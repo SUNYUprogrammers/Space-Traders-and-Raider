030f8e4d19b1687caacbfc88bbaabc747228750a
YsevniaBaladon <54846816+YsevniaBaladon@users.noreply.github.com>
2019-10-10 15:03:53 -0400

    Using Upload, as Kraken no work

---
+++ Space Traders and Raiders/Assets/Scripts/Ship Type Subclasses/Cruiser_Class.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cruiser_Class : Ship_Class
+{
+    private new void Start()
+    {
+        base.Start();
+
+        ship_type = "Cruiser";
+        size = 15;
+        pos = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    private new void Update()
+    {
+        base.Update();
+    }
+}
---
+++ Space Traders and Raiders/Assets/Scripts/Ship Type Subclasses/Destroyer_Class.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer_Class : Ship_Class
+{
+    private new void Start()
+    {
+        base.Start();
+
+        ship_type = "Destroyer";
+        size = 10;
+        pos = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    private new void Update()
+    {
+        base.Update();
+    }
+}
