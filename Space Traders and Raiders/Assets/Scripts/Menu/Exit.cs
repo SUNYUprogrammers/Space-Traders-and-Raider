3911fe43ddec75a29cd5b4a8e75d9d0931800b3e
Dullis <plimccormack@gmail.com>
2019-10-17 19:02:35 -0400

    Added Menus.
    To get the menus to work properly you have to build it

---
+++ Space Traders and Raiders/Assets/Scripts/Menu/Exit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        Text inFunc = GameObject.Find("exit").GetComponent<Text>();
+        inFunc.onClick.AddListener(TasksOnClick);
+    }
+
+    void TasksOnClick()
+    {
+        Debug.Log("Exited!");
+        //Application.Quit();
+    }*/
+
+    void OnMouseDown()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
---
+++ Space Traders and Raiders/Assets/Scripts/Menu/NewGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+    //private AssetBundle myLoadedAssetBundle;
+    //private string[] scenePaths;
+
+    /*
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes/Compile");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+    */
+    void OnMouseDown()
+    {
+        print("New game");
+        SceneManager.LoadScene("Oct18");
+    }
+}
