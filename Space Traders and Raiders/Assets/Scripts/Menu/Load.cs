839b0d97ba17e762b14b470bfa152f0bc2818711
Jacob <plimccormack@gmail.com>
2019-10-18 13:23:55 -0400

    Menu changes
    Generally made the menus look better also added debug text for "Load" and "Options"

---
+++ Space Traders and Raiders/Assets/Scripts/Menu/Load.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Load : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("To be made");
+    }
+}
---
+++ Space Traders and Raiders/Assets/Scripts/Menu/Options.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print("TBD. Goes to overlay possibly another scene");
+    }
+}
