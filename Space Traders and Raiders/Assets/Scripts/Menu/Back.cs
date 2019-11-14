e2a4ed8f09096a72a5501861ee87b067f1a487d0
Jacob <plimccormack@gmail.com>
2019-11-13 20:18:40 -0500

    Changed how the menus work

--- Space Traders and Raiders/Assets/Scripts/Menu/Back.cs
+++ Space Traders and Raiders/Assets/Scripts/Menu/Back.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 
 public class Back : MonoBehaviour
 {
@@ -16,5 +17,24 @@ public class Back : MonoBehaviour
         Main.enabled = true;
         OCamera.enabled = false;
         LoadC.enabled = false;
+=======
+using UnityEngine.SceneManagement;
+
+public class Back : MonoBehaviour
+{
+    //Camera OCamera;
+    //Camera Main;
+    //Camera LoadC;
+    private void OnMouseDown()
+    {
+        //print("Back");//This is left in for future debugging
+        //OCamera = GameObject.Find("OptionCamera").GetComponent<Camera>();
+        //Main = GameObject.Find("Main Camera").GetComponent<Camera>();
+        //LoadC = GameObject.Find("LoadCamera").GetComponent<Camera>();
+        //Main.enabled = true;
+        //OCamera.enabled = false;
+        //LoadC.enabled = false;
+        SceneManager.LoadScene("MainMenu");
+>>>>>>> Stashed changes
     }
 }
