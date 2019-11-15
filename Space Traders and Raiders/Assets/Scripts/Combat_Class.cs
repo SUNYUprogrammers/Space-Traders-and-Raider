919318a428235c10c4704aab6d9d67f7dcfb1266
Thecontrollers <mandudegaming@GMAIL.COM>
2019-11-15 17:11:36 -0500

    Combat is part way  finished
    Needs more work to swap between, code will work for more then frigates

--- Space Traders and Raiders/Assets/Scripts/Combat_Class.cs
+++ Space Traders and Raiders/Assets/Scripts/Combat_Class.cs
@@ -21,8 +21,11 @@ public class Combat_Class : MonoBehaviour
     private Ship_Class defender;
     public Canvas CombatUI;
     public Canvas Askcombat;
-    bool combat = false;;
+    bool combat = false;
     bool Switch = false;
+
+    int numofbeam, numofmissle, numofsheild, numofantimissle = 0;
+
     public void Start()
     {
         gm = GameObject.FindObjectOfType<GameManager>(); 
@@ -94,6 +97,7 @@ public class Combat_Class : MonoBehaviour
 
     public void EndCombat()
     {
+        //if player == current.faction 
         CombatUI.enabled = false;
     }
 
@@ -146,18 +150,117 @@ public class Combat_Class : MonoBehaviour
         print("does something still thinking bout it");
     }
 
+
+    // Engine -- ingore
+    //Armour -- TARGET FIRST 
+    //Beam -- 1 beam = 1 attack,2 beam = 2 attack etc
+    //Sheild -- protects againist lasers 50% of blocking
+    //Missile -- 1 missle = 1 attack, etc
+    //Coloization Module -- ingore
+    //Anti-Missile -- protects againist missiles 50% of blocking
+    //System Repair Module --write last check GDD
+
+    //do math after both sides attack
+
+
+
+
+
+
     public void Update()
     {
         //print("update combat");
         if (combat)
         {
             
-            if (attacker != null && defender != null && Switch == false)
+            if (attacker != null && defender != null && Switch == false)//Attacker Turn(Initante Combat)
             {
-                print(attacker.name + " destroys a compenent " + defender.name);
+                FindWeapons(attacker.parts_list);
+                FindDefense(defender.parts_list);
+                for (int i = 0; i <= (numofbeam + numofmissle); i++)
+                {
+                    if (Random.Range(0, 100) <= 50)//chance of attacking, if its less then 50
+                    {
+                        if(numofbeam != 0)//beam attack                        
+                        {
+                            if(numofsheild != 0)//do they have sheild
+                            {
+                                if(Random.Range(0,100) >= 50)//50% of blocking, if its more then 50 then block 
+                                {
+                                    print("Damage was blocked");
+                                    numofbeam--;
+                                    numofsheild--;
+                                }
+                                else//attacked
+                                {
+                                    print("Damage was done");
+                                    numofbeam--;
+                                    numofsheild--;
+                                }
+                            }
+
+                        }
+                        if(numofmissle != 0)
+                        {
+
+                        }
+                    }
+                }
+            }       
+
+        }
+    }
+
+
+
+    public void FindDefense(Component_Class[] parts)//find sheild
+    {
+        foreach(Component_Class i in parts)
+        {
+            if (i != null)
+            {
+                print(i.getType());
+                if (i.getType() == "Sheild")
+                {
+                    numofsheild++;
+                }
+                if (i.getType() == "Anti Missle")
+                {
+                    numofantimissle++;
+                }
             }
         }
     }
+
+    public void FindWeapons(Component_Class[] parts)
+    {
+        foreach (Component_Class i in parts)
+        {
+            if (i != null)
+            {
+                print(i.getType());
+                if (i.getType() == "Beam")
+                {
+                    numofbeam++;
+                }
+                if (i.getType() == "Missile")
+                {
+                    numofmissle++;
+                }
+            }
+        }
+    }
+
+
 }
+
+   
+
+
+
+
+
+
+
 
 
