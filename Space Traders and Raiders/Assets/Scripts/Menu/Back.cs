60a9e920968348e587f0cc67900badbb414a1764
HarlanJ <sparkyjohn.harlan@gmail.com>
2019-11-14 20:29:17 -0500

    Brought Combat stuffs into Nov 15
    I think I did it right?

--- Space Traders and Raiders/Assets/Scripts/Menu/Back.cs
+++ Space Traders and Raiders/Assets/Scripts/Menu/Back.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
 
 public class Back : MonoBehaviour
 {
@@ -17,24 +16,5 @@ public class Back : MonoBehaviour
         Main.enabled = true;
         OCamera.enabled = false;
         LoadC.enabled = false;
-=======
-using UnityEngine.SceneManagement;
-
-public class Back : MonoBehaviour
-{
-    //Camera OCamera;
-    //Camera Main;
-    //Camera LoadC;
-    private void OnMouseDown()
-    {
-        //print("Back");//This is left in for future debugging
-        //OCamera = GameObject.Find("OptionCamera").GetComponent<Camera>();
-        //Main = GameObject.Find("Main Camera").GetComponent<Camera>();
-        //LoadC = GameObject.Find("LoadCamera").GetComponent<Camera>();
-        //Main.enabled = true;
-        //OCamera.enabled = false;
-        //LoadC.enabled = false;
-        SceneManager.LoadScene("MainMenu");
->>>>>>> Stashed changes
     }
 }
--- Space Traders and Raiders/Assets/Scripts/Menu/Options.cs
+++ Space Traders and Raiders/Assets/Scripts/Menu/Options.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
 
 public class Options : MonoBehaviour
 {
@@ -14,21 +13,5 @@ public class Options : MonoBehaviour
         Main = GameObject.Find("Main Camera").GetComponent<Camera>();
         Main.enabled = false;
         OCamera.enabled = true;
-=======
-using UnityEngine.SceneManagement;
-
-public class Options : MonoBehaviour
-{
-    //Camera OCamera;
-    //Camera Main;
-    private void OnMouseDown()
-    {
-        //print("Options");//This is left in for future debugging
-        //OCamera = GameObject.Find("OptionCamera").GetComponent<Camera>();
-        //Main = GameObject.Find("Main Camera").GetComponent<Camera>();
-        //Main.enabled = false;
-        //OCamera.enabled = true;
-        SceneManager.LoadScene("OptionMenu");
->>>>>>> Stashed changes
     }
 }
