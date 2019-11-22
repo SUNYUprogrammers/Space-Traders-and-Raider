08ad35a98f899c62e2ff81a1a5cbddcf14729f70
Thecontrollers <mandudegaming@GMAIL.COM>
2019-11-22 13:38:58 -0500

    cyka byant

---
+++ Space Traders and Raiders/Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Canvas menu;
+
+    private Ship_Class[] array;
+    int num;
+
+    public void Combat()
+    {
+        Combat_Class bat = GameObject.FindObjectOfType<Combat_Class>();
+        bat.Combat(array, num);
+        Cancel();
+    }
+
+    public void Trade()
+    {
+        //rons crap
+        Cancel();
+    }
+
+    public void Cancel()
+    {
+        menu.enabled = false;
+    }
+
+    public void Menu(Ship_Class[] shipsInStack,int len)
+    {
+        menu.enabled = true;
+        array = shipsInStack;
+        num = len;
+        
+    }
+}
